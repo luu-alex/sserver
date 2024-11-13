215126c516d216cfa9b5a3a9e80acb1bfdcc50c2
luu-alex <98a.lexluu@gmail.com>
2024-11-13 11:13:32 +0700

    change back

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,7 @@ import { basicChest } from './items';
 const app = express();
 const PORT = process.env.PORT || 5001;
 // Middleware
-app.use(cors({
-  origin: 'https://shinyknights.netlify.app/',
-}));
+app.use(cors());
 app.use(express.json());
 connect();
 
